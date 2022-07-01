d72ee949d9451a75646ec606e154faa632610316
Subhaditya Bhattacharjee <82825565+Subhaditya16@users.noreply.github.com>
2022-07-01 11:49:02 +0530

    Update settings.py

--- nielitpro1/settings.py
+++ nielitpro1/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ffx$b_br@h9ceho4%--j&#rg(q%vk-43n&0rq()&te6_ow)4@t'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
